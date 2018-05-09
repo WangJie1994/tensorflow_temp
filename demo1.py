9d3fad78e9d2be6ab0961cac0fbd111101febeb1
Jie Wang <wangjie19940416@icloud.com>
2018-05-09 13:11:32 +0800

    Initial commit

---
+++ demo1.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May  7 19:31:15 2018
+
+@author: wangj
+"""
+
+import tensorflow as tf
+import numpy as np
+
+
+x_data = np.random.rand(100).astype(np.float32)
