0bf7a3eae8341ccd19f8dece552391409017b281
Jie Wang <wangjie19940416@icloud.com>
2018-05-09 13:14:43 +0800

    test

--- demo1.py
+++ demo1.py
@@ -8,5 +8,9 @@ Created on Mon May  7 19:31:15 2018
 import tensorflow as tf
 import numpy as np
 
-
+#create data
 x_data = np.random.rand(100).astype(np.float32)
+y_data = x_data*0.1 + 0.3
+
+#create tensorflow structure
+
