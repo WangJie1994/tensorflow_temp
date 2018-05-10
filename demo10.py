43d3e86a0907096e185e8cdfba03a2838dc7225c
Jie Wang <wangjie19940416@icloud.com>
2018-05-10 15:48:26 +0800

    bug fix

--- demo10.py
+++ demo10.py
@@ -8,8 +8,8 @@ mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
 def compute_accuracy(v_xs, v_ys):
     global prediction
     y_pre = sess.run(prediction, feed_dict={xs: v_xs, keep_prob: 1})
-    correct_prediction = tf.equal(tf.arg_max(y_pre, 1), tf.arg_max(v_ys, 1))
-    accuracy = tf.reduce_min(tf.cast(correct_prediction, tf.float32))
+    correct_prediction = tf.equal(tf.argmax(y_pre, 1), tf.argmax(v_ys, 1))
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
     result = sess.run(accuracy, feed_dict={xs: v_xs, ys: v_ys, keep_prob: 1})
     return result
 
@@ -77,6 +77,6 @@ with tf.Session() as sess:
 
     for i in range(1000):
         batch_xs, batch_ys = mnist.train.next_batch(100)
-        sess.run(train_step, feed_dict={xs:batch_xs, ys:batch_ys, keep_prob:0.5})
+        sess.run(train_step, feed_dict={xs: batch_xs, ys: batch_ys, keep_prob: 0.5})
         if i%50 == 0:
             print(compute_accuracy(mnist.test.images, mnist.test.labels))
